0384847d7f33e820cbb594353797345d61e3ed8a
Vesa Halttunen <vesa@framestream.nl>
2016-01-12 14:19:16 +0100

    Initial Commit

---
+++ Oddstream/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Oddstream
+//
+//  Created by Vesa Halttunen on 12-01-16.
+//  Copyright © 2016 FrameStream. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
