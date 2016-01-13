01236798f7ed077909ff501e9f49141d21d611cf
Vesa Halttunen <vesa@framestream.nl>
2016-01-13 14:47:35 +0100

    Add back button to video view

--- Oddstream/VideoContentViewController.swift
+++ Oddstream/VideoContentViewController.swift
@@ -12,4 +12,8 @@ class VideoContentViewController: UIViewController {
     @IBOutlet weak var currentTimeLabel: UILabel!
     @IBOutlet weak var totalTimeLabel: UILabel!
     @IBOutlet weak var timeSlider: UISlider!
+    
+    @IBAction func back(_: AnyObject) {
+        navigationController?.popToViewController((navigationController?.viewControllers[(navigationController?.viewControllers.count)! - 3])!, animated: true)
+    }
 }
