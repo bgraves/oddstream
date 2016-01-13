79d94683cc2a2943a107be3ef70ec90445d723b1
Vesa Halttunen <vesa@framestream.nl>
2016-01-13 18:44:44 +0100

    Load tour contents based on its ID

--- Oddstream/SelectTourViewController.swift
+++ Oddstream/SelectTourViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class SelectTourViewController: UIViewController {
-    var tours: NSArray? = []
+    var tours: Array<Dictionary<String, AnyObject>> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,21 +18,19 @@ class SelectTourViewController: UIViewController {
         
         let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
             do {
-                self.tours = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSArray
+                self.tours = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as! Array<Dictionary<String, AnyObject>>
                 
-                if self.tours != nil && self.tours!.count > 0 {
+                if self.tours.count > 0 {
                     dispatch_async(dispatch_get_main_queue(), {
                         var index = 0
-                        var y = self.view.bounds.size.height / 2 - CGFloat(self.tours!.count * 50) - CGFloat((self.tours!.count - 1) * 25)
+                        var y = self.view.bounds.size.height / 2 - CGFloat(self.tours.count * 50) - CGFloat((self.tours.count - 1) * 25)
                         
-                        for tour in self.tours! {
-                            let tourette: NSDictionary = tour as! NSDictionary
-                            
+                        for tour in self.tours {
                             let button = UIButton.init(type: UIButtonType.Custom)
                             button.tag = index++
                             button.frame = CGRect(x: self.view.bounds.size.width * 0.11, y: y, width: self.view.bounds.size.width * 0.78, height: 100)
                             button.titleLabel?.font = UIFont.init(name: "Lekton-Bold", size: 24)
-                            button.setTitle(tourette["title"] as? String, forState: UIControlState.Normal)
+                            button.setTitle(tour["title"] as? String, forState: UIControlState.Normal)
                             button.setTitleColor(UIColor(red:0xf7 / 255.0, green:0xe3 / 255.0, blue:0xdd / 255.0, alpha: 1.0), forState: UIControlState.Normal)
                             button.layer.borderWidth = 2
                             button.layer.borderColor = UIColor(red:0xf7 / 255.0, green:0xe3 / 255.0, blue:0xdd / 255.0, alpha: 1.0).CGColor
@@ -52,12 +50,13 @@ class SelectTourViewController: UIViewController {
     }
     
     @IBAction func setTour(button: UIButton) {
-        performSegueWithIdentifier("ShowWalkAroundViewController", sender: self.tours![button.tag])
+        performSegueWithIdentifier("ShowWalkAroundViewController", sender: self.tours[button.tag])
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         let walkAroundViewController = segue.destinationViewController as! WalkAroundViewController
-        walkAroundViewController.tour = sender as? NSDictionary
+        let tour = sender as! Dictionary<String, AnyObject>
+        walkAroundViewController.tourId = tour["id"] as! Int
     }
 }
 
--- Oddstream/WalkAroundViewController.swift
+++ Oddstream/WalkAroundViewController.swift
@@ -10,18 +10,31 @@ import UIKit
 
 class WalkAroundViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
-    var tour: NSDictionary? = nil
-    
+    var tourId: Int = 0
+    var tour: Dictionary<String, AnyObject> = [:]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.imageView.image = UIImage.animatedImageNamed("Oddstream-illustration-step", duration: 4.0)
+
+        let url = NSURL(string: "http://oddstream.miraclethings.nl/api/oddstream/tour?id=\(tourId)")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+            do {
+                self.tour = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as! Dictionary<String, AnyObject>
+            } catch {
+                
+            }
+        }
+        
+        task.resume()
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
-        performSegueWithIdentifier("ShowCheckBeaconViewController", sender: self)
+//        performSegueWithIdentifier("ShowCheckBeaconViewController", sender: self)
     }
 
     @IBAction func back(_: AnyObject) {
