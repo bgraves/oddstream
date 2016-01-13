f6c2ff0cd8db1c63bd3b804f393cd418d2127828
Vesa Halttunen <vesa@framestream.nl>
2016-01-13 14:15:20 +0100

    View for playing back audio content

--- Oddstream/CheckBeaconViewController.swift
+++ Oddstream/CheckBeaconViewController.swift
@@ -27,6 +27,6 @@ class CheckBeaconViewController: UIViewController {
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
-        performSegueWithIdentifier("ShowImageContentViewController", sender: self)
+        performSegueWithIdentifier("ShowAudioContentViewController", sender: self)
     }
 }
