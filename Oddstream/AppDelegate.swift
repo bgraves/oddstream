046c8df032f5eec6e5c81961a2899302ddf05df3
Vesa Halttunen <vesa@framestream.nl>
2016-01-20 16:21:21 +0100

    Scale the background image to the portrait screen size

--- Oddstream/AppDelegate.swift
+++ Oddstream/AppDelegate.swift
@@ -16,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         if let image = UIImage(named: "Oddstream-background") {
-            UIGraphicsBeginImageContext(CGSize(width: 1.0, height: (window?.bounds.size.height)!))
-            image.drawInRect(CGRect(x: 0.0, y: 0.0, width: 1.0, height: (window?.bounds.size.height)!))
+            UIGraphicsBeginImageContext(CGSize(width: 1.0, height: UIScreen.mainScreen().nativeBounds.size.height))
+            image.drawInRect(CGRect(x: 0.0, y: 0.0, width: 1.0, height: UIScreen.mainScreen().nativeBounds.size.height))
             window!.backgroundColor = UIColor(patternImage: UIGraphicsGetImageFromCurrentImageContext())
             UIGraphicsEndImageContext();
         }
